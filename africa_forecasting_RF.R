6172e4609b855ee58be7edd58cac16d9d00d8c72
alinajibpour <ali.najibpour68@gmail.com>
2024-06-02 19:14:56 +0200

    changes

--- africa_forecasting_RF.R
+++ africa_forecasting_RF.R
@@ -5,28 +5,6 @@ setwd("/dss/dssfs04/lwp-dss-0002/pn98na/pn98na-dss-0000/racek/CNNs_for_conflict_
 est_df = readRDS(paste0("data/est_df_", cell_size ,".rds"))
 
 
-est_df = est_df %>% 
-  #mutate(log_pop = log(pop)) %>% 
-  mutate(ucdp1_log_lag_1 = log(ucdp1_lag_1 + 1), ucdp2_log_lag_1 = log(ucdp2_lag_1 + 1), 
-         ucdp1_log_12lags = log(ucdp1_12lags + 1), ucdp2_log_12lags = log(ucdp2_12lags + 1)) %>% 
-  select(id, time_point, intersect_area, long, lat, pop,
-         landcover_grass_shrub, landcover_bare, landcover_built, landcover_crop, landcover_tree, landcover_water,
-         ucdp1_bin,
-         ucdp1_log_lag_1, ucdp2_log_lag_1,
-         ucdp1_log_12lags, ucdp2_log_12lags,
-         #ucdp1_last_conflict, ucdp2_last_conflict,
-         month, year
-  ) %>% 
-  mutate(obs_id = row_number()) %>% 
-  relocate(obs_id)
-
-write.csv(est_df %>% select(-pop, -starts_with(("landcover_"))), "data/cnn_est_df.csv")
-
-
-
-
-
-
 task = as_task_classif(est_df, target = "ucdp1_bin", positive = "1")  
 task$select(setdiff(task$feature_names, c("id", "time_point", "month", "year")))
 
