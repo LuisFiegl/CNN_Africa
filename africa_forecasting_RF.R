3b32b54b99f86dd000bf165254885fb14a746fd6
alinajibpour <ali.najibpour68@gmail.com>
2024-06-06 17:31:16 +0200

    first try

--- africa_forecasting_RF.R
+++ africa_forecasting_RF.R
@@ -1,19 +1,21 @@
 library(tidyverse)
 library(mlr3verse)
 library(ranger)
-setwd("/dss/dssfs04/lwp-dss-0002/pn98na/pn98na-dss-0000/racek/CNNs_for_conflict_pred/Syria")
-est_df = readRDS(paste0("data/est_df_", cell_size ,".rds"))
+est_df = readRDS(paste0("./est_df.rds"))
+est_df = est_df %>%
+  filter(year <2020) %>%
+  filter(year >2014) %>%
+  mutate(obs_id = row_number())
 
-
-task = as_task_classif(est_df, target = "ucdp1_bin", positive = "1")  
-task$select(setdiff(task$feature_names, c("id", "time_point", "month", "year")))
+task = as_task_classif(est_df, target = "ucdp_12_bin_next_year", positive = "1")  
+task$select(setdiff(task$feature_names, c("id", "month", "year")))
 
 rf_model = lrn("classif.ranger",predict_type = "prob")
 rf_model$param_set$values = list(num.trees = 500, num.threads = 16)
 
 
-train_ids = est_df %>% filter(year < 2018) %>% pull(obs_id)
-test_ids = est_df %>% filter(year >= 2018) %>% pull(obs_id)
+train_ids = est_df %>% filter(year < 2019) %>% pull(obs_id)
+test_ids = est_df %>% filter(year >= 2019) %>% pull(obs_id)
 
 rf_model$train(task, row_ids = train_ids)
 
@@ -23,7 +25,7 @@ prediction_df = as.data.table(prediction_rf) %>%
   rename(pred_rf = prob.1)
 
 
-saveRDS(prediction_df, "results/prediction_df.rds")
+#saveRDS(prediction_df, "results/prediction_df.rds")
 
 
 prediction_rf$score(list(msr("classif.acc"),
