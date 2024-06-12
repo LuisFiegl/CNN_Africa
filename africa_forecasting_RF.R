dd2644569576aae7c2164418a567a5f38b4ebfd1
alinajibpour <ali.najibpour68@gmail.com>
2024-06-12 17:55:42 +0200

    .

--- africa_forecasting_RF.R
+++ africa_forecasting_RF.R
@@ -3,19 +3,19 @@ library(mlr3verse)
 library(ranger)
 est_df = readRDS(paste0("./est_df.rds"))
 est_df = est_df %>%
-  filter(year <2020) %>%
-  filter(year >2014) %>%
+  filter(year <=2020) %>%
+  filter(year >2015) %>%
   mutate(obs_id = row_number())
 
-task = as_task_classif(est_df, target = "ucdp_12_bin_next_year", positive = "1")  
+task = as_task_classif(est_df, target = "ucdp_12_bin", positive = "1")  
 task$select(setdiff(task$feature_names, c("id", "month", "year")))
 
 rf_model = lrn("classif.ranger",predict_type = "prob")
 rf_model$param_set$values = list(num.trees = 500, num.threads = 16)
 
 
-train_ids = est_df %>% filter(year < 2019) %>% pull(obs_id)
-test_ids = est_df %>% filter(year >= 2019) %>% pull(obs_id)
+train_ids = est_df %>% filter(year <= 2019) %>% pull(obs_id)
+test_ids = est_df %>% filter(year > 2019) %>% pull(obs_id)
 
 rf_model$train(task, row_ids = train_ids)
 
